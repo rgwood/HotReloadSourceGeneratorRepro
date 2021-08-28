91a7ddb3265d64f6eada9b0f42acfa97c2ac3e2b
reilly.wood <reilly.wood@icloud.com>
2021-08-28 10:38:21 -0700

    Add project files.

---
+++ HotReloadRepro/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HotReloadRepro;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using Windows.Win32;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        while (true)
+        {
+            // this breaks: use GetCurrentThreadId from the source-generated code
+            uint threadId = PInvoke.GetCurrentThreadId();
+
+            // this works: use a regular PInvoke, no source generator
+            //uint threadId = GetCurrentThreadId();
+
+            Console.WriteLine($"Hello from thread {threadId}");
+            await Task.Delay(1000);
+        }
+    }
+
+    // PInvoke definition copied directly from CsWin32 generated source
+    [DllImport("Kernel32", ExactSpelling = true)]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+    [SupportedOSPlatform("windows5.1.2600")]
+    internal static extern uint GetCurrentThreadId();
+}
